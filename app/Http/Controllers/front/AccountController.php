f7278b8a2781bb8b2010bc419af9426a80e43bc9
Vansh Srivastav <vanshsrivastavjnp25@gmail.com>
2025-12-23 22:24:33 +0530

    Complete Register & User Authentication

---
+++ app/Http/Controllers/front/AccountController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\front;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    public function register(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ];
+        $validator = Validator::make($request->all(),$rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = 'customer';
+        $user->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'You have Register Successfully.'
+        ], 200);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            
+            return response()->json([
+                'status' => 400,
+                'error' => $validator->errors()
+            ],400);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            
+            $user = User::find(Auth::user()->id);
+
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200, 
+                'token' => $token,
+                'id' => $user->id,
+                'name' => $user->name, 
+            ],200);
+
+        } else {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Either email/password is incorrect.' 
+            ],401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\front\AccountController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::post('/admin/login', [AuthController::class, 'authenticate']);
+
+Route::post('register', [AccountController::class, 'register']);
+Route::post('login', [AccountController::class, 'authenticate']);
 
 Route::group(['middleware' => 'auth:sanctum'],function(){
     Route::resource('categories', CategoryController::class);
